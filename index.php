38cd7cb678e7fea74763ba813e1ece206c47e7d4
AlejandroGaspar <alex_gaspar_10@live.com.mx>
2014-02-05 20:50:52 -0600

    programa index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+CTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin tŒ·ŒıŒ·tulo</title>
+</head>
+
+<body background="fondo.jpg">
+<h1><center><font size="+6" color="#000000">Empresa Soriana S.A de C.V</font></center></h1>
+<h2><center><font size="+6" color="#666666">Departamentos</font></center></h2>
+<?php
+echo "<p>Nombre del departamento: $dep";
+echo "<p>No. de departamento: $nomdep";
+echo "<p>Empleado: $em";
+echo "<p>Horario: $hor";
+echo "<p>Materia Prima: $mapr";
+echo "<p>Producto Terminado: $pr";
+echo "<p>Producto Almacen: $alm";
+echo "<p>Embasado: $emba";
+echo "<p>Embalaje: $eml";
+echo "<p>Area: $area";
+echo "<p>Horas Trabajadas: $hrs";
+echo "<p>Existencia: $ex";
+echo "<p>Tipo: $tip";
+echo "<p>Marca: $mar";
+echo "<p>Codigo: $ap";
+
+?>
+</body>
+</html>
+
